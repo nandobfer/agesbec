0ae1498bc95fc9c05178ca64e80bf027408b6922
nandobfer <nandobfer@yahoo.com.br>
2022-09-12 17:50:10 -0300

    started sql server handling

--- src/database_handler.py
+++ src/database_handler.py
@@ -1,9 +1,28 @@
 from burgos.mysql_handler import Mysql
+import pypyodbc as sqlServer
+import json
+
+config = json.load(open('config.json'))
+sql_server_config = config['databases']['collect_acessos']
 
 class Database():
     def __init__(self):
-        self.collect = Mysql()
+        self.collect = sqlServer.connect(connection_string)
         self.processed = Mysql()
 
 
+DRIVER_NAME = 'SQL SERVER'
+SERVER_NAME = sql_server_config['host']
+DATABASE_NAME = sql_server_config['database']
+USER_NAME = sql_server_config['user']
+PASSWORD = sql_server_config['password']
+
+connection_string = f"""
+    DRIVER={{{DRIVER_NAME}}};
+    SERVER={SERVER_NAME};
+    DATABASE={DATABASE_NAME};
+    Trust_Connection=yes;
+    uid={USER_NAME};
+    pwd={PASSWORD};
+"""
 
