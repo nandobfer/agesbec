b74b990af1d551d42f8566d7889360474b6e410b
nandobfer <nandobfer@yahoo.com.br>
2022-09-21 00:20:57 -0300

    test: test

--- test.py
+++ test.py
@@ -2,3 +2,6 @@ from src.database_handler import Database
 
 database = Database()
 print(database.collect)
+
+cursor = database.cursor()
+cursor.execute('select Nome from Acessos where nome like "%fabio%"')
