59ac090c21c00dc0a4dc1d25cb0c019ef1f6b875
nandobfer <nandobfer@yahoo.com.br>
2022-11-07 18:06:34 -0300

    test: time

--- src/Receita.py
+++ src/Receita.py
@@ -18,8 +18,8 @@ class Receita():
         self.buildAPIAttributes()
         
     def buildDate(self, date, _time):
-        formated_datetime = f'{date}T{_time}.000{config["timezone"]}'
-        formated_datetime = formated_datetime[:-9]+formated_datetime[-6:]
+        formated_datetime = f'{date}T{_time}{config["timezone"]}'
+        # formated_datetime = formated_datetime[:-9]+formated_datetime[-6:]
         print(f'dataHoraOcorrencia: {formated_datetime}')
         return formated_datetime
     
