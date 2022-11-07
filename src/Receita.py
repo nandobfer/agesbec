a3e6782c52a3a618d12ef2730e30ed9d92603e92
nandobfer <nandobfer@yahoo.com.br>
2022-11-07 15:02:57 -0300

    test: endpoint

--- src/Receita.py
+++ src/Receita.py
@@ -15,7 +15,7 @@ class Receita():
     def __init__(self, acesso, saida = False):
         self.acesso = acesso
         self.saida = saida
-        self.endpoint = "/sapi/ext/acesso-pessoas"
+        self.endpoint = "sapi/ext/acesso-pessoas"
         
         if saida:
             self.data = acesso.data_saida
