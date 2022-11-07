a197dccce55f93e6494daeb3c017a71a084da1b4
nandobfer <nandobfer@yahoo.com.br>
2022-11-07 18:49:30 -0300

    test: sql when there is no previous row

--- src/Acesso.py
+++ src/Acesso.py
@@ -46,14 +46,11 @@ class Acesso():
             sql = f"insert into {processed_db['table']} {columns} values ({self.id}, '{self.nome}', '{self.cpf}', '{self.data_entrada}', '{self.hora_entrada}', '{self.data_saida}', '{self.hora_saida}', '{request}');"
         else:
             sql = f'SELECT * FROM {processed_db["table"]} WHERE id = {self.id} AND data_saida like "%None%"'
-            print(sql)
             exists = self.database.processed.run(sql)
             if exists:
                 sql = f'UPDATE {processed_db["table"]} SET data_saida = "{self.data_saida}", hora_saida = "{self.hora_saida}", status = "{request}" WHERE id = {self.id}'
-                print(sql)
             else:
-                print('aqui')
-                return False
+                sql = f"insert into {processed_db['table']} {columns} values ({self.id}, '{self.nome}', '{self.cpf}', '{self.data_entrada}', '{self.hora_entrada}', '{self.data_saida}', '{self.hora_saida}', '{request}');"
         
         try:
             print(sql)
