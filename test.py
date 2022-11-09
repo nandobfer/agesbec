777d017802029237141bd5223b8c1dd97383974c
nandobfer <nandobfer@yahoo.com.br>
2022-11-09 14:35:27 -0300

    test: mockado

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ class Mockado():
     def __init__(self) -> None:
         self.data_entrada = datetime.now().date()
         self.hora_entrada = datetime.now().time()
-        self.nome = 'Edinaldo Bueno Costa'
+        self.nome = 'Fernando Burgos'
         self.id = 1
-        self.cpf = '14254811837'
+        self.cpf = '02576698506'
         self.identificacao = '1'
         
 teste = Receita(Mockado())
