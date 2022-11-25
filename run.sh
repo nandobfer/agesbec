1de48ce9ff0ad36b9270029a1d8797edb5077056
nandobfer <nandobfer@yahoo.com.br>
2022-11-25 17:02:17 -0300

    update: run script starts a screen bound to python

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@
 
 if [[ $# == 0 ]]
 then
-    python3 app.py
+    screen -m -d -S siscomex python3 app.py
 fi
 
 if [[ $1 ]]
 then
     if [[ $1 == '-u' ]]
     then
-        git stash -u && git pull && chmod +x run.sh && python3 app.py
+        git stash -u && git pull && chmod +x run.sh && screen -m -d -S siscomex python3 app.py
     fi
 fi
