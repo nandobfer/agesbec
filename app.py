3dcb977c0cb8f6a854f74887521b5a1a81c21347
nandobfer <nandobfer@yahoo.com.br>
2022-11-27 11:10:34 -0300

    test: response mysql

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def collectData(saida = False):
         tipo = 'saida'
         sql = f'SELECT * FROM {config["databases"]["processed_acessos"]["table"]} ORDER BY data_{tipo} DESC, hora_{tipo} DESC LIMIT 20'
     try:
-        sql = f'SELECT TOP 1 * FROM {config["databases"]["collect_acessos"]["table"]} ORDER BY data_{tipo} DESC, hora_{tipo} DESC'
+        sql = f'SELECT TOP 1 * FROM {config["databases"]["collect_acessos"]["table"]} ORDER BY data_{tipo} DESC, hora_{tipo} DESC LIMIT 10'
         acessos = database.collect.query(sql)['results']
         for item in acessos:
             acesso = Acesso(item, database)
