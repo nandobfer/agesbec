0ee5d1564c0ab3161848906a4be381a7a5abb698
nandobfer <nandobfer@yahoo.com.br>
2022-11-27 12:00:45 -0300

    teste: mysql

--- src/Acesso.py
+++ src/Acesso.py
@@ -55,7 +55,6 @@ class Acesso():
                 values = (self.id, self.nome, self.cpf, self.data_entrada, self.hora_entrada, self.data_saida, self.hora_saida, request)
         
         try:
-            print(sql)
             self.database.processed.run(sql, prepared=True, values=values)
             print(datetime.now().time())
             print(f'processed id {self.id}, name: {self.nome}, {"saida" if saida else "entrada"}')
