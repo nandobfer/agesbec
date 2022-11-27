e8ad8716e871b79d82f25721d1d3a2b84546bffc
nandobfer <nandobfer@yahoo.com.br>
2022-11-26 21:38:47 -0300

    test: token

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from datetime import datetime, timezone
 from src.Receita import Receita
 class Mockado():
     def __init__(self) -> None:
-        self.data_entrada = datetime.now().date()
-        self.hora_entrada = datetime.now().time()
+        self.data_entrada = str(datetime.now().date())
+        self.hora_entrada = str(datetime.now().time())
         self.nome = 'Fernando Burgos'
         self.id = 1
         self.cpf = '02576698506'
