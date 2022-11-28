bfa3beaabaffb278b8a3b03796d12ef8770a31e5
nandobfer <nandobfer@yahoo.com.br>
2022-11-28 16:20:23 -0300

    test: loop

--- src/Acesso.py
+++ src/Acesso.py
@@ -32,22 +32,23 @@ class Acesso():
             sql = f"select * from {processed_db['table']} where cpf = '{self.cpf}' and data_{tipo} = '{self.data_saida}' and hora_{tipo} = '{self.hora_saida}';"
         else:
             sql = f"select * from {processed_db['table']} where cpf = '{self.cpf}' and data_{tipo} = '{self.data_entrada}' and hora_{tipo} = '{self.hora_entrada}';"
+        print(sql)
         data = self.database.processed.run(sql)
         if data:
+            print(data)
             return True
         else:
             return False
         
     def process(self, saida = False):
+        request = json.dumps(Receita(self, saida).requestAcesso())
         columns = '(id, nome, cpf, data_entrada, hora_entrada, data_saida, hora_saida, status)'
         if not saida:
-            request = json.dumps(Receita(self, saida).requestAcesso())
             sql = f"""insert into {processed_db["table"]} {columns} values (%s,%s,%s,%s,%s,%s,%s,%s);"""
             values = (self.id, self.nome, self.cpf, self.data_entrada, self.hora_entrada, self.data_saida, self.hora_saida, request)
         else:
             sql = f"""SELECT * FROM {processed_db["table"]} WHERE id = {self.id} AND data_saida like '%None%' """
             exists = self.database.processed.run(sql)
-            request = json.dumps(Receita(self, saida).requestAcesso())
             if exists:
                 sql = f"""UPDATE {processed_db["table"]} SET data_saida = %s, hora_saida = %s, status = %s WHERE id = %s"""
                 values = (self.data_saida, self.hora_saida, request, self.id)
