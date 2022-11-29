656b1cee3fc5f80f2af1e8f172532ad03cc61fc4
nandobfer <nandobfer@yahoo.com.br>
2022-11-29 13:08:16 -0300

    test: credenciamento

--- app.py
+++ app.py
@@ -30,8 +30,9 @@ def collectCredenciamento():
         sql = f'SELECT TOP 1 * FROM {config["databases"]["collect_funcionarios"]["table"]}'
         funcionarios = funcionarios_db.collect.query(sql)['results']
         for item in funcionarios:
-            funcionario = Funcionario(item, funcionarios_db)
+            print(item)
             
+            funcionario = Funcionario(item, funcionarios_db)
             if not funcionario.isProcessed():
                 funcionario.process()
 
