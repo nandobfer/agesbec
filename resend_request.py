052f3a284da25432136485353bbee6852ab3897b
nandobfer <nandobfer@yahoo.com.br>
2022-11-28 22:53:17 -0300

    test: resend

--- resend_request.py
+++ resend_request.py
@@ -3,6 +3,10 @@ from src.Receita import Receita
 
 if len(sys.argv) > 1:
     acesso = sys.argv[1]
+    
+    print(acesso)   
+    sys.stdout.flush()
 
     with open('test.txt', 'w') as f:
         f.write(str(acesso))
+     
