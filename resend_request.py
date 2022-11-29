104d570f4a5ef00ca35ce4fad298b912bd41cbb4
nandobfer <nandobfer@yahoo.com.br>
2022-11-28 23:27:23 -0300

    test: resend

--- resend_request.py
+++ resend_request.py
@@ -2,11 +2,9 @@ import sys, json
 from src.Receita import Receita
 
 if len(sys.argv) > 1:
-    request_str = sys.argv[1]
-    acesso = json.loads(request_str)
-
-    print(acesso)
+    acesso = sys.argv[1]
     
+    print(acesso)   
     
     sys.stdout.flush()
      
