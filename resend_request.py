123807df35df1fa235b3e581a6091755b5d17853
nandobfer <nandobfer@yahoo.com.br>
2022-11-28 23:29:43 -0300

    test: resend

--- resend_request.py
+++ resend_request.py
@@ -4,8 +4,11 @@ from src.Receita import Receita
 if len(sys.argv) > 1:
     acesso = sys.argv[1]
     
-    print(acesso)   
-    print(type(acesso))
+    try:
+        acesso = json.loads(acesso)
+        print(acesso)   
+    except Exception as error:
+        print(error)
     
     sys.stdout.flush()
      
