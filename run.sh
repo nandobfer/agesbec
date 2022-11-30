4d4b5addc361b1a34990dcff9b924e6eed32e20d
nandobfer <nandobfer@yahoo.com.br>
2022-11-30 20:38:51 -0300

    fix: run script

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 if [[ $# == 0 ]]
 then
     screen -m -d -S siscomex python3 controle_acesso.py
-    screen -m -d -S siscomex python3 credenciamento_pessoas.py
+    screen -m -d -S credenciamento python3 credenciamento_pessoas.py
 fi
 
 if [[ $1 ]]
@@ -11,10 +11,11 @@ then
     if [[ $1 == '-u' ]]
     then
         screen -X -S siscomex kill
+        screen -X -S credenciamento kill
         git stash -u
         git pull
         chmod +x run.sh
         screen -m -d -S siscomex python3 controle_acesso.py
-        screen -m -d -S siscomex python3 credenciamento_pessoas.py
+        screen -m -d -S credenciamento python3 credenciamento_pessoas.py
     fi
 fi
