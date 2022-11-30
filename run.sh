7831469a42792de06ead1d9c0a311a4ccce08671
nandobfer <nandobfer@yahoo.com.br>
2022-11-30 20:17:57 -0300

    update: added credenciamento to run script

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 if [[ $# == 0 ]]
 then
-    screen -m -d -S siscomex python3 app.py
+    screen -m -d -S siscomex python3 controle_acesso.py
+    screen -m -d -S siscomex python3 credenciamento_pessoas.py
 fi
 
 if [[ $1 ]]
@@ -13,6 +14,7 @@ then
         git stash -u
         git pull
         chmod +x run.sh
-        screen -m -d -S siscomex python3 app.py
+        screen -m -d -S siscomex python3 controle_acesso.py
+        screen -m -d -S siscomex python3 credenciamento_pessoas.py
     fi
 fi
