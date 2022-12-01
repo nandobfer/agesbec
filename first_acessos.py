177cf994c6272ac514c0f333689e030acbb045c4
nandobfer <nandobfer@yahoo.com.br>
2022-11-30 21:35:57 -0300

    test: first acessos

--- first_acessos.py
+++ first_acessos.py
@@ -25,6 +25,7 @@ def collectAcessos(saida = False):
     try:
         sql = f'SELECT * FROM {config["databases"]["collect_acessos"]["table"]} ORDER BY data_{tipo} DESC, hora_{tipo} DESC'
         acessos = database.collect.query(sql)['results']
+        print(acessos)
         for item in acessos:
             acesso = Acesso(item, database)
             if not acesso.isProcessed(saida = saida):
